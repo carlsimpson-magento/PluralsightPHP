e4ed765d1bda48d59b5b85b0ab355cb826462bb6
Carl Simpson <carl.simpson@maginus.com>
2020-03-21 17:54:27 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlsimpson
+ * Date: 13/03/2020
+ * Time: 22:11
+ */
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="web/css/style.css">
+</head>
+    <body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+        <section class="homepage-title">
+            <h1 class="title">Pluralsight PHP Coursework</h1>
+            <p class="title"> 4 week Block </p>
+        </section>
+
+
+
+
+
+    </body>
+</html>
